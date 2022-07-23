8b648a862d6363c36b4f16188b5819913b38341f
ishwang <his8457@naver.com>
2022-07-23 16:05:16 +0900

    #10 - 도메인 코드를 엔티티로 변환
    * 1 도메인 클래스 == 1 테이블 매칭되도록 설계함
    * 생성자와 'equals()', 'hashcode()' 등이 도메인 맥락에 맞도록 자세히 설정함.

--- fastcampus-project-board/src/main/java/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/fastcampus/projectboard/domain/Article.java
@@ -1,16 +1,89 @@
 package fastcampus.projectboard.domain;
 
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Table(indexes = {
+		@Index(columnList = "title"),
+		@Index(columnList = "hashtag"),
+		@Index(columnList = "createdAt"),
+		@Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
 	
-	private long id;
-	private String title; //제목
-	private String content; //본문
-	private String hashtag; //해시태그
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
-	private String creatAt; //생성자
-	private Date createBy; //생성일시
-	private String modifiedAt; //수정자
-	private Date modifiedBy; //수정일시
+	@Setter @Column(nullable = false) private String title; //제목
+	@Setter @Column(nullable = false, length = 10000) private String content; //본문
+	
+	@Setter private String hashtag; //해시태그
+	
+	@ToString.Exclude
+	@OrderBy("id")
+	@OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+	private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+	
+	@CreatedDate @Column(nullable = false) private Date createdAt; //생성일시
+	@CreatedBy @Column(nullable = false, length = 100) private String createdBy; //생성자
+	@LastModifiedDate @Column(nullable = false) private Date modifiedAt; //수정일시
+	@LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; //수정자
+	
+	protected Article() {}
+
+	public Article(String title, String content, String hashtag) {
+		this.title = title;
+		this.content = content;
+		this.hashtag = hashtag;
+	}
+	
+	public static Article of(String title, String content, String hashtag) {
+		return new Article(title, content, hashtag);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Article)) {
+			return false;
+		}
+		Article other = (Article) obj;
+		return id != null && Objects.equals(id, other.id);
+	}
 }
--- fastcampus-project-board/src/main/java/fastcampus/projectboard/domain/ArticleComment.java
+++ fastcampus-project-board/src/main/java/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,16 +1,76 @@
 package fastcampus.projectboard.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Table(indexes = {
+		@Index(columnList = "content"),
+		@Index(columnList = "createdAt"),
+		@Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class ArticleComment {
 	
-	private long id;
-	private Article article; //게시글 (id)
-	private String title; //제목
-	private String content; //본문
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
-	private String creatAt; //생성자
-	private Date createBy; //생성일시
-	private String modifiedAt; //수정자
-	private Date modifiedBy; //수정일시
+	@Setter @ManyToOne(optional = false) private Article article; //게시글 (id)
+	@Setter @Column(nullable = false, length = 500) private String content; //본문
+	
+	@CreatedDate @Column(nullable = false) private Date createdAt; //생성일시
+	@CreatedBy @Column(nullable = false, length = 100) private String createdBy; //생성자
+	@LastModifiedDate @Column(nullable = false) private Date modifiedAt; //수정일시
+	@LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; //수정자
+	
+	protected ArticleComment() {}
+
+	public ArticleComment(Article article, String content) {
+		this.article = article;
+		this.content = content;
+	}
+	
+	public static ArticleComment of(Article article, String content) {
+		return new ArticleComment(article, content);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof ArticleComment)) {
+			return false;
+		}
+		ArticleComment other = (ArticleComment) obj;
+		return id != null && Objects.equals(id, other.id);
+	}
 }
