e1c22cee4f54db25e7d019fdff5e05849e790960
ishwang <his8457@naver.com>
2025-02-09 23:30:59 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/jpa/bookmanager/FastcampusJpaBookmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.jpa.bookmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusJpaBookmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusJpaBookmanagerApplication.class, args);
+    }
+
+}
